2c6225243a45f4827be3c62a31419f1eaf32873e
JuanZu77 <juanvzubiri77@gmail.com>
2025-06-21 16:03:29 -0300

    update config properties

--- src/main/java/com/juan_zubiri/monitoreo/ChallengeMonitoreoApplication.java
+++ src/main/java/com/juan_zubiri/monitoreo/ChallengeMonitoreoApplication.java
@@ -2,21 +2,11 @@ package com.juan_zubiri.monitoreo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
-
 
 @SpringBootApplication(scanBasePackages = "com.juan_zubiri.monitoreo")
 public class ChallengeMonitoreoApplication {
 
 	public static void main(String[] args) {
-		
-		// Cargar las variables de entorno desde el archivo .env
-        Dotenv dotenv = Dotenv.load();
-        
-        // Verifica si la variable se cargó correctamente
-        System.out.println("URL:" + dotenv.get("DB_URL"));  // Esto debería imprimir la URL de la base de datos
-        System.out.println("NOMBRE: " + dotenv.get("DB_USERNAME"));
-        System.out.println("PASS: " + dotenv.get("DB_PASSWORD"));
         
 		SpringApplication.run(ChallengeMonitoreoApplication.class, args);
 	}
