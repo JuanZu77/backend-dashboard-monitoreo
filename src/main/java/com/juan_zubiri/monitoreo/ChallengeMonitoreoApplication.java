6af22170c500725d0d921880404015b2fb33f0cf
JuanZu77 <juanvzubiri77@gmail.com>
2025-06-21 18:00:15 -0300

    update final

--- src/main/java/com/juan_zubiri/monitoreo/ChallengeMonitoreoApplication.java
+++ src/main/java/com/juan_zubiri/monitoreo/ChallengeMonitoreoApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = "com.juan_zubiri.monitoreo")
 public class ChallengeMonitoreoApplication {
 
-	public static void main(String[] args) {
-        
+	public static void main(String[] args) {   
 		SpringApplication.run(ChallengeMonitoreoApplication.class, args);
 	}
 
